cca289b857b3b1e0cf63928174c1aadf5ab1d283
fzr850 <64519021+fazar80431@users.noreply.github.com>
2024-05-17 10:03:50 +0800

    Update build-hal.sh
    Changed directory

--- build-hal.sh
+++ build-hal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source halium.env
-cd $ANDROID_ROOT
+cd /home/runner/work/ubports-ci-4a/halium/
 
 source build/envsetup.sh
 export USE_CCACHE=1
@@ -12,6 +12,6 @@ make -j$(nproc) recoveryimage
 make -j$(nproc) systemimage 
 
 echo "md5sum halium-boot.img and system.img"
-md5sum $ANDROID_ROOT/out/target/product/rolex/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/rolex/recovery.img
-md5sum $ANDROID_ROOT/out/target/product/rolex/system.img
+md5sum /home/runner/work/ubports-ci-4a/halium/out/target/product/rolex/halium-boot.img
+md5sum /home/runner/work/ubports-ci-4a/halium/out/target/product/rolex/recovery.img
+md5sum /home/runner/work/ubports-ci-4a/halium/out/target/product/rolex/system.img

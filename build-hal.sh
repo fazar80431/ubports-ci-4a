01defe2f74eb3e8bb632d5c84624f6f25e00e419
fzr850 <64519021+fazar80431@users.noreply.github.com>
2024-05-18 20:59:20 +0800

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -7,11 +7,13 @@ export USE_CCACHE=1
 breakfast $DEVICE
 make -j$(nproc) mkbootimg
 make -j$(nproc) hybris-boot
+make -j$(nproc) hybris-recovery
 make -j$(nproc) halium-boot
 make -j$(nproc) recoveryimage
 make -j$(nproc) systemimage 
 
 echo "md5sum halium-boot.img and system.img"
 md5sum /home/runner/work/ubports-ci-4a/ubports-ci-4a/halium/out/target/product/rolex/halium-boot.img
+md5sum /home/runner/work/ubports-ci-4a/ubports-ci-4a/halium/out/target/product/rolex/halium-recovery.img
 md5sum /home/runner/work/ubports-ci-4a/ubports-ci-4a/halium/out/target/product/rolex/recovery.img
 md5sum /home/runner/work/ubports-ci-4a/ubports-ci-4a/halium/out/target/product/rolex/system.img
